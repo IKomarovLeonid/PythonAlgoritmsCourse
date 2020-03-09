5bbdf898035bd212d02dffd1d5b67e0711b94f59
LeonidK <LeonidK@brokeree.com>
2020-03-09 19:24:20 +0300

    Complete single Linkedlist. All tests passed Add second task: two way Linkedlist Add tests for second task

--- tests.py
+++ tests.py
@@ -1,382 +1,320 @@
 import unittest
-from PythonLinkedlist import *
+from LinkedListTwoWay import *
+
 
 class Test_tests(unittest.TestCase):
-    
-    def test_len_SomeNodes_Equal_4(self):
-        list=LinkedList()
-        n1=Node(12)
-        n2=Node(13)
-        n3=Node(14)
-        n4=Node(-2)
+
+    def When_ListIsEmpty_Len_Should_BeZero(self):
+        list=LinkedList2()
+        self.assertEqual(list.len(),0)
+
+    def When_ListIsNotEmptyAndHasFiveElements_Len_Should_ProvideFiveValue(self):
+        list=LinkedList2()
+        n1=Node(1)
+        n2=Node(2)
+        n3=Node(3)
+        n4=Node(4)
+        n5=Node(5)
+        n1.next=n2
+        n2.prev=n1
+        n2.next=n3
+        n3.prev=n2
+        n3.next=n4
+        n4.prev=n3
+        n4.next=n5
+        n5.prev=n4
         list.add_in_tail(n1)
         list.add_in_tail(n2)
         list.add_in_tail(n3)
         list.add_in_tail(n4)
-        count=list.len()
-        self.assertEqual(count,4)
+        list.add_in_tail(n5)
+        self.assertEqual(list.len(),5)
 
-    def test_Len_Empty_List_Is_0(self):
-        list=LinkedList()
-        count=list.len()
-        self.assertEqual(count,0)
+    def When_ListIsEmpty_AddInHead_MustBeAdded(self):
+        list=LinkedList2()
+        n1=Node(1)
+        list.add_in_head(n1)
+        self.assertEqual(list.len(),1)
+        self.asserEqual(list.head.value,1)
 
-    def test_Clean_Make_List_Empty(self):
-        list=LinkedList()
-        n1=Node(2)
-        n2=Node(4)
-        n3=Node(5)
+    def When_ListIsNotEmpty_AddInHead_MustChangeCurrentHead(self):
+        list=LinkedList2()
+        n1=Node(1)
+        n2=Node(2)
+        n3=Node(3)
+        n4=Node(4)
+        n1.next=n2
+        n2.prev=n1
+        n2.next=n3
+        n3.prev=n2
+        n3.next=n4
+        n4.prev=n3
         list.add_in_tail(n1)
         list.add_in_tail(n2)
         list.add_in_tail(n3)
+        list.add_in_tail(n4)
+        newHead=Node(100)
+        list.add_in_head(newHead)
+        self.assertEqual(list.len(),5)
+        self.asserEqual(list.head.value,100)
+
+    def When_ListIsEmpty_Clean_MustMakeListAllEmpty(self):
+        list=LinkedList2()
         list.clean()
-        self.assertEqual(0,list.len())
+        self.assertEqual(list.len(),0)
+        self.assertEqual(list.head,None)
+        self.assertEqual(list.tail,None)
 
-    def test_Clean_Empty_List_Make_Empty(self):
-        list=LinkedList()
+    def When_ListIsNotEmpty_Clean_MustMakeListEmpty(self):
+        list=LinkedList2()
+        n1=Node(1)
+        n2=Node(2)
+        n3=Node(3)
+        n4=Node(4)
+        n5=Node(5)
+        n1.next=n2
+        n2.prev=n1
+        n2.next=n3
+        n3.prev=n2
+        n3.next=n4
+        n4.prev=n3
+        n4.next=n5
+        n5.prev=n4
+        list.add_in_tail(n1)
+        list.add_in_tail(n2)
+        list.add_in_tail(n3)
+        list.add_in_tail(n4)
+        list.add_in_tail(n5)
         list.clean()
-        self.assertEqual(0,list.len())
+        self.assertEqual(list.len(),0)
+        self.assertEqual(list.head,None)
+        self.assertEqual(list.tail,None)
 
-    def test_insert_after(self):
-        list=LinkedList()
-        n1=Node(3)
-        n2=Node(4)
-        n3=Node(6)
-        n4=Node(-1)
-        list.add_in_tail(n1)
-        list.add_in_tail(n2)
-        list.add_in_tail(n3)
-        list.insert(n2,n4)
-        self.assertEqual(4,list.len())
-
-    def test_insert_after_tail(self):
-        list=LinkedList()
-        n1=Node(3)
-        n2=Node(4)
-        n3=Node(5)
-        n4=Node(0)
-        list.add_in_tail(n1)
-        list.add_in_tail(n2)
-        list.add_in_tail(n3)
-        list.insert(n3,n4)
-        self.assertEqual(4,list.len())
-
-    def test_insert_after_not_exist_node(self):
-        list=LinkedList()
-        n1=Node(5)
-        n2=Node(5)
-        n0=Node(3)
-        n3=Node(5)
-        n4=Node(-1)
-        list.add_in_tail(n1)
-        list.add_in_tail(n2)
-        list.add_in_tail(n3)
-        list.insert(n0,n4)
-        self.assertEqual(3,list.len())
-
-    def test_find_all(self):
-        list=LinkedList()
-        n1=Node(2)
-        n2=Node(1)
+    def When_ListContainsValue_Find_MustReturnItem(self):
+        list=LinkedList2()
+        n1=Node(1)
+        n2=Node(2)
         n3=Node(3)
-        n4=Node(5)
+        n4=Node(4)
         n5=Node(5)
+        n1.next=n2
+        n2.prev=n1
+        n2.next=n3
+        n3.prev=n2
+        n3.next=n4
+        n4.prev=n3
+        n4.next=n5
+        n5.prev=n4
         list.add_in_tail(n1)
         list.add_in_tail(n2)
         list.add_in_tail(n3)
         list.add_in_tail(n4)
         list.add_in_tail(n5)
-        elements=list.find_all(n4.value)
-        self.assertEqual([n4,n5],elements)
+        node=list.find(3)
+        self.assertIsNotNone(node)
+        self.assertEqual(3,node.value)
+        self.assertEqual(list.tail,None)
 
-    def test_find_all_not_exists(self):
-        list=LinkedList()
-        n1=Node(2)
-        n2=Node(1)
+    def When_ListNotContainsValue_Find_MustReturnNone(self):
+        list=LinkedList2()
+        n1=Node(1)
+        n2=Node(2)
         n3=Node(3)
-        n4=Node(5)
+        n4=Node(4)
         n5=Node(5)
+        n1.next=n2
+        n2.prev=n1
+        n2.next=n3
+        n3.prev=n2
+        n3.next=n4
+        n4.prev=n3
+        n4.next=n5
+        n5.prev=n4
         list.add_in_tail(n1)
         list.add_in_tail(n2)
         list.add_in_tail(n3)
         list.add_in_tail(n4)
         list.add_in_tail(n5)
-        elements=list.find_all(-2)
-        self.assertEqual([],elements)
-    
-    def test_find_all_if_all_same_as_list(self):
-        list=LinkedList()
+        node=list.find(-2)
+        self.assertIsNone(node)
+
+    def When_ListNotContainsValue_FindAll_MustReturnEmptyList(self):
+        list=LinkedList2()
         n1=Node(1)
         n2=Node(1)
-        n3=Node(1)
-        n4=Node(1)
-        n5=Node(1)
-        list.add_in_tail(n1)
-        list.add_in_tail(n2)
-        list.add_in_tail(n3)
-        list.add_in_tail(n4)
-        list.add_in_tail(n5)
-        elements=list.find_all(1)
-        self.assertEqual([n1,n2,n3,n4,n5],elements)
-
-    def test_delete_node_single_from_head(self):
-        list=LinkedList()
-        n1=Node(1)
-        n2=Node(2)
-        n3=Node(3)
-        n4=Node(4)
-        n5=Node(5)
-        list.add_in_tail(n1)
-        list.add_in_tail(n2)
-        list.add_in_tail(n3)
-        list.add_in_tail(n4)
-        list.add_in_tail(n5)
-        list.delete(1)
-        self.assertEqual(4,list.len())
-
-    def test_delete_node_single_from_middle(self):
-        list=LinkedList()
-        n1=Node(1)
-        n2=Node(2)
-        n3=Node(3)
-        n4=Node(4)
-        n5=Node(5)
-        list.add_in_tail(n1)
-        list.add_in_tail(n2)
-        list.add_in_tail(n3)
-        list.add_in_tail(n4)
-        list.add_in_tail(n5)
-        list.delete(4)
-        self.assertEqual(4,list.len())
-
-    def test_delete_node_single_from_tail_false(self):
-        list=LinkedList()
-        n1=Node(1)
-        n2=Node(2)
-        n3=Node(3)
-        n4=Node(4)
-        n5=Node(5)
-        list.add_in_tail(n1)
-        list.add_in_tail(n2)
-        list.add_in_tail(n3)
-        list.add_in_tail(n4)
-        list.add_in_tail(n5)
-        list.delete(5)
-        self.assertEqual(4,list.len())
-
-    def test_delete_not_existed_single(self):
-        list=LinkedList()
-        n1=Node(1)
-        n2=Node(2)
-        n3=Node(3)
-        n4=Node(4)
-        n5=Node(5)
-        list.add_in_tail(n1)
-        list.add_in_tail(n2)
-        list.add_in_tail(n3)
-        list.add_in_tail(n4)
-        list.add_in_tail(n5)
-        list.delete(7)
-        self.assertEqual(5,list.len())
-
-    def test_delete_not_existed_All(self):
-        list=LinkedList()
-        n1=Node(1)
-        n2=Node(2)
-        n3=Node(3)
-        n4=Node(4)
-        n5=Node(5)
-        list.add_in_tail(n1)
-        list.add_in_tail(n2)
-        list.add_in_tail(n3)
-        list.add_in_tail(n4)
-        list.add_in_tail(n5)
-        list.delete(7,True)
-        self.assertEqual(5,list.len())
-    
-    def test_delete_all_existed_nodes(self):
-        list=LinkedList()
-        n1=Node(1)
-        n2=Node(2)
-        n3=Node(1)
-        n4=Node(4)
-        n5=Node(1)
-        list.add_in_tail(n1)
-        list.add_in_tail(n2)
-        list.add_in_tail(n3)
-        list.add_in_tail(n4)
-        list.add_in_tail(n5)
-        list.delete(1,True)
-        self.assertEqual(2,list.len())
-
-    def test_delete_single_element(self):
-        list=LinkedList()
-        n1=Node(1)
-        list.add_in_tail(n1)
-        list.delete(1)
-        self.assertEqual(0,list.len())
-
-    def test_delete_single_element_true(self):
-        list=LinkedList()
-        n1=Node(1)
-        list.add_in_tail(n1)
-        list.delete(1,True)
-        self.assertEqual(0,list.len())
-
-    def test_delete_single_at_tail_true(self):
-        list=LinkedList()
-        n1=Node(1)
-        n2=Node(2)
-        n3=Node(3)
-        n4=Node(4)
-        n5=Node(5)
-        list.add_in_tail(n1)
-        list.add_in_tail(n2)
-        list.add_in_tail(n3)
-        list.add_in_tail(n4)
-        list.add_in_tail(n5)
-        list.delete(5,True)
-        self.assertEqual(4,list.len())
-
-    def test_delete_single_at_middle_true(self):
-        list=LinkedList()
-        n1=Node(1)
-        n2=Node(2)
-        n3=Node(3)
-        n4=Node(4)
-        n5=Node(5)
-        list.add_in_tail(n1)
-        list.add_in_tail(n2)
-        list.add_in_tail(n3)
-        list.add_in_tail(n4)
-        list.add_in_tail(n5)
-        list.delete(3,True)
-        self.assertEqual(4,list.len())
-
-    def test_delete_empty_list_true(self):
-        list=LinkedList()
-        list.delete(3,True)
-        self.assertEqual(0,list.len())
-
-    def test_delete_empty_list_false(self):
-        list=LinkedList()
-        list.delete(3)
-        self.assertEqual(0,list.len())
-
-    def test_delete_all_nodes_by_delete_true(self):
-        list=LinkedList()
-        n1=Node(1)
-        n2=Node(1)
-        n3=Node(1)
-        n4=Node(1)
-        n5=Node(1)
-        list.add_in_tail(n1)
-        list.add_in_tail(n2)
-        list.add_in_tail(n3)
-        list.add_in_tail(n4)
-        list.add_in_tail(n5)
-        list.delete(1,True)
-        self.assertEqual(0,list.len())
-
-    def test_delete_almost_nodes_by_delete_true(self):
-        list=LinkedList()
-        n1=Node(1)
-        n2=Node(1)
-        n3=Node(1)
-        n4=Node(1)
-        n5=Node(2)
-        list.add_in_tail(n1)
-        list.add_in_tail(n2)
-        list.add_in_tail(n3)
-        list.add_in_tail(n4)
-        list.add_in_tail(n5)
-        list.delete(1,True)
-        self.assertEqual(1,list.len())
-
-    def test_delete_almost_in_middle(self):
-        list=LinkedList()
-        n1=Node(2)
-        n2=Node(2)
-        n3=Node(1)
-        n4=Node(2)
-        n5=Node(2)
-        list.add_in_tail(n1)
-        list.add_in_tail(n2)
-        list.add_in_tail(n3)
-        list.add_in_tail(n4)
-        list.add_in_tail(n5)
-        list.delete(2,True)
-        self.assertEqual(1,list.len())
-
-    def test_delete_almost_nodes_from_tail_true(self):
-        list=LinkedList()
-        n1=Node(1)
-        n2=Node(2)
         n3=Node(2)
         n4=Node(2)
         n5=Node(2)
+        n1.next=n2
+        n2.prev=n1
+        n2.next=n3
+        n3.prev=n2
+        n3.next=n4
+        n4.prev=n3
+        n4.next=n5
+        n5.prev=n4
         list.add_in_tail(n1)
         list.add_in_tail(n2)
         list.add_in_tail(n3)
         list.add_in_tail(n4)
         list.add_in_tail(n5)
-        list.delete(2,True)
-        list.print_all_nodes()
-        self.assertEqual(1,list.len())
+        collection=list.find_all(-1)
+        self.assertEqual(collection==[],True)
 
-    def test_insert_into_empty_list(self):
-         list=LinkedList()
-         n1=Node(10)
-         list.insert(None,n1)
-         self.assertEqual(1,list.len())
-
-    def test_insert_None(self):
-        list=LinkedList()
-        n1=Node(10)
-        list.add_in_tail(n1)
-        list.insert(n1,None)
-        self.assertEqual(1,list.len())
-
-    def test_insert_in_tail(self):
-        list=LinkedList()
-        n1=Node(10)
-        n2=Node(11)
-        n3=Node(12)
-        n4=Node(-1)
+    def When_ListContainsValuesFromHead_FindAll_MustReturnCorrectElements(self):
+        list=LinkedList2()
+        n1=Node(1)
+        n2=Node(1)
+        n3=Node(1)
+        n4=Node(1)
+        n5=Node(2)
+        n1.next=n2
+        n2.prev=n1
+        n2.next=n3
+        n3.prev=n2
+        n3.next=n4
+        n4.prev=n3
+        n4.next=n5
+        n5.prev=n4
         list.add_in_tail(n1)
         list.add_in_tail(n2)
         list.add_in_tail(n3)
-        list.insert(n3,n4)
-        self.assertEqual(4,list.len())
+        list.add_in_tail(n4)
+        list.add_in_tail(n5)
+        collection=list.find_all(1)
+        self.assertEqual(collection==[1,1,1,1],True)
 
-    def test_insert_in_middle(self):
-        list=LinkedList()
-        n1=Node(10)
-        n2=Node(11)
-        n3=Node(12)
-        n4=Node(-1)
+    def When_ListContainsValuesFromTail_FindAll_MustReturnCorrectElements(self):
+        list=LinkedList2()
+        n1=Node(2)
+        n2=Node(1)
+        n3=Node(1)
+        n4=Node(1)
+        n5=Node(1)
+        n1.next=n2
+        n2.prev=n1
+        n2.next=n3
+        n3.prev=n2
+        n3.next=n4
+        n4.prev=n3
+        n4.next=n5
+        n5.prev=n4
         list.add_in_tail(n1)
         list.add_in_tail(n2)
         list.add_in_tail(n3)
-        list.insert(n1,n4)
-        self.assertEqual(4,list.len())
+        list.add_in_tail(n4)
+        list.add_in_tail(n5)
+        collection=list.find_all(1)
+        self.assertEqual(collection==[1,1,1,1],True)
 
-    def test_insert_after_None(self):
-        list=LinkedList()
-        n1=Node(10)
-        n2=Node(11)
-        n3=Node(12)
-        n4=Node(-1)
+    def When_ListContainsValuesFromMiddle_FindAll_MustReturnCorrectElements(self):
+        list=LinkedList2()
+        n1=Node(2)
+        n2=Node(1)
+        n3=Node(1)
+        n4=Node(1)
+        n5=Node(2)
+        n1.next=n2
+        n2.prev=n1
+        n2.next=n3
+        n3.prev=n2
+        n3.next=n4
+        n4.prev=n3
+        n4.next=n5
+        n5.prev=n4
         list.add_in_tail(n1)
         list.add_in_tail(n2)
         list.add_in_tail(n3)
-        list.insert(None,n4)
-        self.assertEqual(3,list.len())
-        list.print_all_nodes()
+        list.add_in_tail(n4)
+        list.add_in_tail(n5)
+        collection=list.find_all(1)
+        self.assertEqual(collection==[1,1,1],True)
 
-     
+    def When_ListEmpty_Delete_Must_BeCorrect(self):
+        list=LinkedList2()
+        n1=Node(2)
+        list.delete(n1.value)
+        self.assertEqual(list.len(),0)
+        self.assertEqual(list.head,None)
+        self.assertEqual(list.tail,None)
+
+    def When_ListIsNotEmpty_Delete_HeadMustChangeHead(self):
+        list=LinkedList2()
+        n1=Node(1)
+        n2=Node(2)
+        n3=Node(3)
+        n4=Node(4)
+        n5=Node(5)
+        n1.next=n2
+        n2.prev=n1
+        n2.next=n3
+        n3.prev=n2
+        n3.next=n4
+        n4.prev=n3
+        n4.next=n5
+        n5.prev=n4
+        list.add_in_tail(n1)
+        list.add_in_tail(n2)
+        list.add_in_tail(n3)
+        list.add_in_tail(n4)
+        list.add_in_tail(n5)
+        list.delete(n1.value)
+        self.assertEqual(list.len(),4)
+        self.assertEqual(list.head.value,2)
+        self.assertEqual(list.tail.value,5)
+
+    def When_ListIsNotEmpty_Delete_FromTail_Must_ChangeListTail(self):
+        list=LinkedList2()
+        n1=Node(1)
+        n2=Node(2)
+        n3=Node(3)
+        n4=Node(4)
+        n5=Node(5)
+        n1.next=n2
+        n2.prev=n1
+        n2.next=n3
+        n3.prev=n2
+        n3.next=n4
+        n4.prev=n3
+        n4.next=n5
+        n5.prev=n4
+        list.add_in_tail(n1)
+        list.add_in_tail(n2)
+        list.add_in_tail(n3)
+        list.add_in_tail(n4)
+        list.add_in_tail(n5)
+        list.delete(n5.value)
+        self.assertEqual(list.len(),4)
+        self.assertEqual(list.head.value,1)
+        self.assertEqual(list.tail.value,4)
+
+    def When_ListIsNotEmpty_Delete_FromMiddle_Must_ChangeList(self):
+        list=LinkedList2()
+        n1=Node(1)
+        n2=Node(2)
+        n3=Node(3)
+        n4=Node(4)
+        n5=Node(5)
+        n1.next=n2
+        n2.prev=n1
+        n2.next=n3
+        n3.prev=n2
+        n3.next=n4
+        n4.prev=n3
+        n4.next=n5
+        n5.prev=n4
+        list.add_in_tail(n1)
+        list.add_in_tail(n2)
+        list.add_in_tail(n3)
+        list.add_in_tail(n4)
+        list.add_in_tail(n5)
+        list.delete(n3.value)
+        self.assertEqual(list.len(),4)
+        self.assertEqual(list.head.next.next.value,4)
+        self.assertEqual(list.tail.prev.prev.value,2)
+
 
 
 if __name__ == '__main__':
-    unittest.main()
+       unittest.main()
 
